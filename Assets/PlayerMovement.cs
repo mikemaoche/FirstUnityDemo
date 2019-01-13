bee0de15a27669a44c03c26955700ffafb1bf1c1
mikemao11 <maochemike@yahoo.fr>
2019-01-14 12:55:44 +1300

    upload project

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float runSpeed = 300.0F;
+    public float horizontalMove = 0F;
+    public Animator animator;
+
+    public bool jump = false;
+    public bool crouch = false;
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+    }
+
+    void FixedUpdate()
+    {
+        controller = GetComponent<CharacterController2D>();
+        // vertical and horizontal
+        controller.Move(horizontalMove * Time.fixedDeltaTime * runSpeed, crouch, jump);
+        jump = false;
+    }
+}
+
+
